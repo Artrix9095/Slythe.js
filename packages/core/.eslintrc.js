5b912f7c0e1d020543ccb2a8a3b1f9b5cc4682da
Artrix <39530102+Artrix9095@users.noreply.github.com>
2022-09-21 13:51:55 -0700

    🛠 Fix Eslint not finding errors in CI

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
   extends: '../../.eslintrc.js',
+  overrides: [
+    {
+      files: ['*.{ts,tsx}'],
+      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+      overrides: [],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  ],
 };
