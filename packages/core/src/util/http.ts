9be49b66815726548c1cdb3760d2bf3dea728bb0
Artrix <39530102+Artrix9095@users.noreply.github.com>
2022-10-10 11:33:00 -0700

    ☄ Add rate limiting handler and add http middleware

--- packages/core/src/util/http.ts
+++ packages/core/src/util/http.ts
@@ -2,8 +2,8 @@
  * @internal
  */
 
-import { APIVersion, RouteBases } from 'discord-api-types/v10';
-import fetch from 'node-fetch';
+import { RouteBases } from 'discord-api-types/v10';
+import fetch, { Response } from 'node-fetch';
 type RequestOptions = {
     body?: any;
     headers?: any;
@@ -12,9 +12,13 @@ type RequestOptions = {
 const USER_AGENT = `Slythe.js (${global.LIB_VERSION}, https://github.com/Artrix9095/Slythe.js)`;
 const DISCORD_API_URL = RouteBases.api;
 
+type APIResponse = Response & {
+    __base: { url: string; method: string; opts: any };
+};
+
 const handleUrl = (url: string) => (url.startsWith('/') ? DISCORD_API_URL + url : url);
 
-const handleBody = (body?: any) => {
+const handleBody = (body?: any): [body: any, isJSON: boolean] => {
     let isJSON = false;
     if (!body) return [undefined, false];
 
@@ -27,6 +31,39 @@ const handleBody = (body?: any) => {
     }
     return [res, isJSON];
 };
+
+export const loggerMiddleware = (res: APIResponse) => {
+    logger.debug(
+        `Received status code [${res.status}] from ${res.url.replace(
+            DISCORD_API_URL,
+            ''
+        )} `
+    );
+    return res;
+};
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+export const errorMiddleware = async (res: APIResponse): Promise<APIResponse> => {
+    if (res.status === 429) {
+        // rate limited
+        const json = await res.json();
+        logger.warn(`You're being rate limited for ${json.retry_after} seconds`);
+        await sleep(json.retry_after * 1000 + 100);
+
+        return await REQUEST(res.__base.url, res.__base.method, res.__base.opts);
+    } else if (res.status >= 400) {
+        const error = await res.json();
+        throw new Error(error.message);
+    } else {
+        return res;
+    }
+};
+// This function is needed so the request can be retried during rate limiting
+const responseMiddleware = (data: { url: string; method: string; opts: any }) => {
+    return (res: Response): APIResponse => Object.assign(res, { __base: data });
+};
+
 //TODO: fix DRY code
 //TODO: implement rate limiting control
 
@@ -42,7 +79,7 @@ export const REQUEST = (
     const headers = {
         'User-Agent': USER_AGENT,
         Authorization: `Bot ${global.token}`,
-        
+
         ...(isJSON ? { 'Content-Type': 'application/json' } : {}),
         ...opts?.headers,
     };
@@ -53,7 +90,10 @@ export const REQUEST = (
         method,
         headers,
         body,
-    });
+    })
+        .then(responseMiddleware({ url, method, opts }))
+        .then(loggerMiddleware)
+        .then(errorMiddleware);
 };
 
 export const GET = (url: string, opts: Partial<RequestOptions> = {}) =>
