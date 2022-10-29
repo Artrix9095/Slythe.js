90608cb5d456b2c628cfdc767a3d31dd52806a2d
Artrix <39530102+Artrix9095@users.noreply.github.com>
2022-10-28 18:09:26 -0700

    Revert "☄ Docs"
    This reverts commit a338305c5bb48449c627e9f9e7e3088ef2b4d531.

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\Student\\Desktop\\Code\\Slythejs\\Slythe.js\\node_modules\\.pnpm\\infima@0.2.0-alpha.42\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\Student\\Desktop\\Code\\Slythejs\\Slythe.js\\node_modules\\.pnpm\\@docusaurus+theme-classic@2.1.0_mjb3abnop6izgvkouxh4aqdnvm\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\Student\\Desktop\\Code\\Slythejs\\Slythe.js\\node_modules\\.pnpm\\@docusaurus+theme-classic@2.1.0_mjb3abnop6izgvkouxh4aqdnvm\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\Student\\Desktop\\Code\\Slythejs\\Slythe.js\\docs\\src\\css\\custom.css'),
-];
