1b3319e6f8175000dc92a30f68385f177886cb52
Artrix <39530102+Artrix9095@users.noreply.github.com>
2022-11-11 01:44:38 +0000

    ❌ Remove server functionality (add to another package later)

--- packages/core/src/lib/Client.ts
+++ packages/core/src/lib/Client.ts
@@ -1,8 +1,6 @@
 import * as DAPI from 'discord-api-types/v10';
 import GatewayHandler from './base/GatewayHandler';
-import { fastify, FastifyReply, FastifyRequest, HookHandlerDoneFunction } from 'fastify';
-import { verifyKey } from 'discord-interactions';
-import { InteractionResponseType, InteractionType, Routes } from 'discord-api-types/v10';
+import { Routes } from 'discord-api-types/v10';
 import { GET } from '../util/http';
 import { Channel } from './structures/Channel';
 import { LevelWithSilent } from 'pino';
@@ -23,7 +21,6 @@ const IS_PROD = process.env.NODE_ENV === 'production';
 
 export class Client extends GatewayHandler {
     public bot: DAPI.APIUser | null = null;
-    public server = fastify({ logger: this.logger });
     protected publicKey = '';
     protected clientId = '';
     constructor(config: ClientConfig) {
@@ -37,39 +34,7 @@ export class Client extends GatewayHandler {
             if (!this.publicKey) {
                 throw new Error('Cannot start server without public key');
             }
-            this.server.listen({ port: (process.env.PORT as any) || 8080 });
         }
-        this.server.addHook('preHandler', this._verifyDiscord);
-    }
-    protected _verifyDiscord(
-        req: FastifyRequest,
-        res: FastifyReply,
-        done: HookHandlerDoneFunction
-    ) {
-        const sig = req.headers['X-Signature-Ed25519'] as string;
-        const timestamp = req.headers['X-Signature-Timestamp'] as string;
-
-        const isValid = verifyKey(
-            JSON.stringify(req.body),
-            sig,
-            timestamp,
-            this.publicKey
-        );
-
-        const error: any = !isValid ? new Error('Invalid request') : null;
-
-        error && (error.statusCode = 400);
-
-        done(error);
-    }
-    protected _handleServer() {
-        this.server.post('/', async (req, res) => {
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            const { type, id, data } = req.body as any;
-
-            if (type === InteractionType.Ping)
-                return res.send({ type: InteractionResponseType.Pong });
-        });
     }
     protected override _onReady({ d: data }: DAPI.GatewayReadyDispatch) {
         this.isReady = true;
@@ -88,10 +53,9 @@ export class Client extends GatewayHandler {
         return GET(Routes.guild(id)).then(data => new Guild(data));
     }
     /**
-     * Kills the bot and host
+     * Kills the bot
      */
     async kill() {
-        await this.server.close().catch();
         this.disconnect();
     }
 }
